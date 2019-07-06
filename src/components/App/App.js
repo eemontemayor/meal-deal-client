04aedb9c78164fccf62f8a163f78de24d2380b5d
Enrique Montemayor <eemontemayor@live.com>
2019-07-06 16:15:17 -0500

    boilerplate

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import React, { Component } from 'react'
+import { Route, Switch } from 'react-router-dom';
+
+export default class App extends Component{
+  render(){
+    return(
+      <div>hello</div>
+    )
+  }
+
+}
