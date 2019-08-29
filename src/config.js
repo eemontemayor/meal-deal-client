b97607b2e1896cfc0478e58f1858dfefa7df8ca0
Enrique Montemayor <eemontemayor@live.com>
2019-08-29 17:33:35 -0500

    x

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 export default {
    
-   API_ENDPOINT:'http://localhost:8000/api', 
+  //  API_ENDPOINT:'http://localhost:8000/api', 
+   API_ENDPOINT:'https://boiling-caverns-54232.herokuapp.com/api', 
     TOKEN_KEY: 'capstone-client-auth-token',
     API_KEY: process.env.REACT_APP_API_KEY,
     APP_ID:process.env.REACT_APP_ID,
