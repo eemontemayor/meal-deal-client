43d533b190c13316501b304983cac80d40d3bdf3
Enrique Montemayor <eemontemayor@live.com>
2019-10-08 20:25:47 -0500

    x

--- src/components/Utils/PrivateRoute.js
+++ src/components/Utils/PrivateRoute.js
@@ -15,7 +15,7 @@ export default function PrivateRoute({ component, ...props }) {
               : (
                 <Redirect
                   to={{
-                    pathname: userContext.user.idle ? '/login' : '/',
+                    pathname: userContext.user.idle ? '/login' : '/register',
                     state: { from: componentProps.location },
                   }}
                 />
