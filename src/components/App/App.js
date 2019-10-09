28dc0348f5e74432cd271b1ab6279d9c27b0024d
Enrique Montemayor <eemontemayor@live.com>
2019-10-08 20:07:06 -0500

    x

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,7 @@
 
 import './App.css';
 import React, { Component } from 'react'
-import { Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import PrivateRoute from '../Utils/PrivateRoute';
 import PublicOnlyRoute from '../Utils/PublicOnlyRoute';
 import LoginPage from '../../routes/LoginPage.js';
@@ -46,7 +46,7 @@ export default class App extends Component{
               path={'/register'}
               component={RegistrationPage}
             />
-             <PrivateRoute
+             <Route
               exact path={'/'}
               component={PlannerPage}
             />
