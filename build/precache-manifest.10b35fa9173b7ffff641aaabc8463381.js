af4bbbcf2036a2f63e15ae7690f075a523df8a3f
Enrique Montemayor <eemontemayor@live.com>
2019-10-11 19:10:04 -0500

    x

---
+++ build/precache-manifest.10b35fa9173b7ffff641aaabc8463381.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04b8bb30155b3d138a0810a86240a75f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e76855748adf630dbf4",
+    "url": "/static/css/2.c51b6406.chunk.css"
+  },
+  {
+    "revision": "872b2b1b523980ddf7a9",
+    "url": "/static/css/main.fa25c752.chunk.css"
+  },
+  {
+    "revision": "3e76855748adf630dbf4",
+    "url": "/static/js/2.dc18cc5b.chunk.js"
+  },
+  {
+    "revision": "872b2b1b523980ddf7a9",
+    "url": "/static/js/main.9368af5b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
