3a8a60fcabaf42cdcb5eb33e411e417ab6687ce9
Enrique Montemayor <eemontemayor@live.com>
2019-11-08 18:28:47 -0600

    fixing landing page button width

--- src/routes/LandingPage/LandingPage.js
+++ src/routes/LandingPage/LandingPage.js
@@ -5,18 +5,18 @@ export default class LandingPage extends Component{
 render(){
     return(
         <div className='landing-page'>
-            <Link to='/planner' >
+            <Link to='/planner' className='landing-page-link'>
                 <button className='landing-btn planner-btn'>
                     Plan
                 </button> 
             </Link>
-            <Link to='/explore' >
+            <Link to='/explore' className='landing-page-link'>
             <button className='landing-btn explore-btn'>
                Explore
                
             </button> 
             </Link>
-            <Link to='/shoppinglist' >
+            <Link to='/shoppinglist'className='landing-page-link' >
                 <button className='landing-btn shoplist-btn'>
                     Shopping List
                 </button> 
