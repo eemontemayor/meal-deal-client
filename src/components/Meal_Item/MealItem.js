6aab258b56912558765b00b522ae9a83a92d35be
Enrique Montemayor <eemontemayor@live.com>
2020-03-14 20:46:18 -0500

    bookmark comp in plannr page mostly done with styling

--- src/components/Meal_Item/MealItem.js
+++ src/components/Meal_Item/MealItem.js
@@ -31,17 +31,17 @@ renderButtons=()=>{
   
   if(this.props.view === 'meals-of-day'){
     return (
-      <div className='item-buttons'>
+      <div >
         <button className='idel-btn'onClick={()=>this.context.handleDeleteMeal(this.props.meal,this.props.index)} >x</button>
-        <Link to={`/meal/${this.context.formattedDate}/${this.props.meal.id}`}><button className='see-more-btn'>see more</button></Link>
+        <Link to={`/meal/${this.context.formattedDate}/${this.props.meal.id}`}><button className='see-more-btn'>...</button></Link>
         <button className='bm-btn' onClick={() => this.context.handleAddBookmark(this.props.meal)}>b</button>
       </div>)
   }
   if(this.props.bookMark){
     return(
-      <div className='item-buttons'>
+      <div className='item-buttons' >
         <button className='del-btn'onClick={()=>this.context.handleDeleteBookmark(this.props.meal,this.props.index)} > <FontAwesomeIcon className='icon trash' size='1x' icon='trash-alt' /></button> 
-        <Link to={`/bookmark/${this.props.meal.id}`}> <button className='see-more-btn'>see more</button>  </Link> {/* have to pass this.props.meal.id here to avoid pkey constraint between md and bm */}
+        <Link to={`/bookmark/${this.props.meal.id}`}> <button className='see-more-btn'>...</button>  </Link> {/* have to pass this.props.meal.id here to avoid pkey constraint between md and bm */}
         <button className='add-btn' onClick={()=>this.context.postMeal(this.props.meal)}> <FontAwesomeIcon className='icon plus' size='1x' icon='plus' /></button> 
       </div> 
       
