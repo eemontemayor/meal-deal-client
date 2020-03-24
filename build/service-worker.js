beb792cc8fe360144cff1591df3c5f8db2229a10
Enrique Montemayor <eemontemayor@live.com>
2020-03-24 17:02:11 -0500

    fixed dimension on planner page to avoid scroll behaviour

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9f8f9465e551e0754d3bc1c9842e7d41.js"
+  "/precache-manifest.72499aee8b688803d9a2bf55036827b1.js"
 );
 
 self.addEventListener('message', (event) => {
